c0bcd23125df2e5b19499c19d6687a11e1c72449
go away <shutup@git.hub>
2025-06-20 01:20:27 +0000

    [automatic commit] 2025-06-20 01:20:27 UTC 14.203.77.55

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -4,7 +4,7 @@ var db = require('../db');
 
 /* GET home page. */
 router.get('/dogs', async(req, res) => {
-  const [rows] = await db.query(`SELECT * FROM Dogs`);
+  const [rows] = await db.query(`SELECT dog_name, size, owner_ FROM Dogs`);
   res.json(rows);
 });
 
