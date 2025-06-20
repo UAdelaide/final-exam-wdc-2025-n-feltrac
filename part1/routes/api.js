85a4793fe3fc80b638b759cdab3bb0dba3ef2a2a
go away <shutup@git.hub>
2025-06-20 01:09:22 +0000

    [automatic commit] 2025-06-20 01:09:22 UTC 14.203.77.55

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -4,7 +4,13 @@ var db = require('../db');
 
 /* GET home page. */
 router.get('/dogs', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const [rows] = await db.query(`
+    SELECT bl.BookID, bi.Title, u.Name AS SellerName, bl.SellerID
+    FROM BookListings bl
+    JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
+    JOIN Users u ON bl.SellerID = u.UserID
+  `);
+  res.json(rows);
 });
 
 module.exports = router;
