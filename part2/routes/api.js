980734dbdf959abcdf5400319f3b88d23add134e
go away <shutup@git.hub>
2025-06-20 06:49:25 +0000

    [automatic commit] 2025-06-20 06:49:25 UTC 14.203.77.55

---
+++ part2/routes/api.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+
+/* GET home page. */
+router.get('/dogs', async(req, res) => {
+  const [rows] = await db.query(`
+    SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs
+    INNER JOIN Users ON Dogs.owner_id = Users.user_id`);
+  res.json(rows);
+});
+
+router.get('/walkrequests/open', async(req, res) => {
+  const [rows] = await db.query(`
+    SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time,
+    WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
+    FROM WalkRequests JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+    JOIN Users ON Dogs.owner_id = Users.user_id
+    WHERE WalkRequests.status = 'open'`);
+  res.json(rows);
+});
+
+router.get('/walkers/summary', async(req, res) => {
+  const [rows] = await db.query(`
+    SELECT Users.username AS walker_username,
+    COUNT(SELECT * FROM WalkRatings INNER JOIN Users ON WalkRatings.walker_id = Users.user_id
+    WHERE Users.role = "walker" AND uhhhhhhhhhhhhh ),
+    avg AS average_rating, completed_walks
+    FROM WalkRequests INNER JOIN Users ON Dogs.owner_id = Users.user_id
+    WHERE User.role = 'walker'`);
+  res.json(rows);
+});
+
+module.exports = router;
