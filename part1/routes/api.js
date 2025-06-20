df402e1a6cdfec37f8ebe10cf78ac917b5327362
go away <shutup@git.hub>
2025-06-20 02:22:20 +0000

    [automatic commit] 2025-06-20 02:22:20 UTC 14.203.77.55

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -12,11 +12,11 @@ router.get('/dogs', async(req, res) => {
 
 router.get('/walkrequests/open', async(req, res) => {
   const [rows] = await db.query(`
-    SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
-      FROM WalkRequests wr
-      JOIN Dogs d ON wr.dog_id = d.dog_id
+    SELECT WalkRequests.*, d.name AS dog_name, d.size, u.username AS owner_name
+      FROM WalkRequests WalkRequests
+      JOIN Dogs d ON WalkRequests.dog_id = d.dog_id
       JOIN Users u ON d.owner_id = u.user_id
-      WHERE wr.status = 'open'`);
+      WHERE WalkRequests.status = 'open'`);
   res.json(rows);
 });
 
