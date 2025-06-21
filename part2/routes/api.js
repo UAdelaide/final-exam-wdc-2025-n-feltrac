8447ee004fda698a25a1afc9fd203be0880818b7
go away <shutup@git.hub>
2025-06-21 04:07:33 +0000

    [automatic commit] 2025-06-21 04:07:33 UTC 14.203.77.55

--- part2/routes/api.js
+++ part2/routes/api.js
@@ -2,12 +2,19 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models/db');
 
-/* GET home page. */
 router.get('/dogs', async(req, res) => {
-  const [rows] = await db.query(`
+  try {
+    const [rows] = await db.query(`
     SELECT Dogs.dog_id, Dogs.name AS dog_name, Dogs.size, Users.user_id AS owner_id FROM Dogs
     INNER JOIN Users ON Dogs.owner_id = Users.user_id`);
-  res.json(rows); // send returned data
+    if(rows.length === 0) {
+      res.json({ message: "no dogs found in database!" });
+    } else {
+      res.json(rows);
+    }
+  } catch (error) {
+    res.json({ message: "an error occurred" });
+  }
 });
 
 module.exports = router;
