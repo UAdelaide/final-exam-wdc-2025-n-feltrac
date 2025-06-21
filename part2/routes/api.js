46a56541c3da000ea01cc651bff8b484063b8205
go away <shutup@git.hub>
2025-06-21 04:08:33 +0000

    [automatic commit] 2025-06-21 04:08:33 UTC 14.203.77.55

--- part2/routes/api.js
+++ part2/routes/api.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var db = require('../models/db');
 
 router.get('/dogs', async(req, res) => {
-  try {
+  try { // query changed slightly from part 1 to include dog_id
     const [rows] = await db.query(`
     SELECT Dogs.dog_id, Dogs.name AS dog_name, Dogs.size, Users.user_id AS owner_id FROM Dogs
     INNER JOIN Users ON Dogs.owner_id = Users.user_id`);
-    if(rows.length === 0) {
-      res.json({ message: "no dogs found in database!" });
+    if(rows.length === 0) { // if there are no rows returned
+      res.json({ message: "no dogs found in database!" }); // no dogs were found
     } else {
       res.json(rows);
     }
