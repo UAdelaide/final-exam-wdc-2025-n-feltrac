79b1de150904af40627fc89b6bffe089c3c1f2db
go away <shutup@git.hub>
2025-06-21 01:31:58 +0000

    [automatic commit] 2025-06-21 01:31:58 UTC 14.203.77.55

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -9,6 +9,8 @@ router.get('/dogs', async(req, res) => {
     SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs
     INNER JOIN Users ON Dogs.owner_id = Users.user_id`);
   res.json(rows);
+  } catch (error) {
+    
   }
 });
 
